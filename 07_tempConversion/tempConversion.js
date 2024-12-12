6df481c8e2e701b6f3d25f5d00ef962d582cc489
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-12 18:02:48 +0000

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function formatNumber(number) {
+  if (Number.isInteger(number)) return number;
+  else return +number.toFixed(1);
+}
+
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  return formatNumber(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  return formatNumber(fahrenheit);
 };
 
 // Do not edit below this line
