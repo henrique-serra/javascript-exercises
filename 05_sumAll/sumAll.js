8e8596f8d23b96955f6e749341697ebb39a68c19
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-12 17:44:55 +0000

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+function isNegative(number) {
+    return number < 0;
+}
 
+function isInteger(number) {
+    return Math.floor(number) === number;
+}
+
+const sumAll = function(a, b) {
+    if ( 
+        (isNegative(a) || isNegative(b)) 
+        || ( !(isInteger(a)) || !(isInteger(b))) ) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let arr = [a, b];
+    arr.sort((a, b) => a - b);
+    let smallNum = arr[0];
+    let largeNum = arr[1];
+    for (let index = smallNum; index <= largeNum; index++) {
+        sum += index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
