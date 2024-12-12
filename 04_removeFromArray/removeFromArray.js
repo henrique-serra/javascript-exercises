07f30038ec85447c432d0659887cb8a2f7211955
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-12 16:11:25 +0000

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    let newArray = [...array];
+    for (const elementToRemove of elementsToRemove) {
+        newArray = newArray.filter(element => element !== elementToRemove)
+    }
+    return newArray
 };
 
 // Do not edit below this line
