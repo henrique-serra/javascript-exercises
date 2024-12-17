cedee98fc074751ac9eb47cd5d01af8e081b7a4c
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-17 18:05:16 +0000

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let formatedString = [];
+    Array.from(string).forEach(character => {
+        const lettersRegex = new RegExp(/[A-Za-z0-9]/);
+        if ( lettersRegex.test(character) ) {
+            formatedString.push(character.toLowerCase());
+        }
+    });
+    let reversedFormatedStr = Array.from(formatedString).reverse();
+    return formatedString.join("") == reversedFormatedStr.join("");
 };
 
 // Do not edit below this line
