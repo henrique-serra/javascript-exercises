9ea955fd9d8d4dd0a477e91b7932332afdd2ae46
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-17 18:20:00 +0000

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (typeof member === "string") {
+        member = Number(member);
+    }
+    if (member == 0) return 0;
+    if (member < 0) return "OOPS";
+    let fibonacci = [1, 1];
+    for (let index = 2; index < member; index++) {
+        fibonacci.push(fibonacci[index - 1] + fibonacci[index - 2])
+    }
+    return fibonacci[member - 1];
 };
 
 // Do not edit below this line
