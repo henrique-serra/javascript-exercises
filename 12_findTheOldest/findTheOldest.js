6b697badd55b6c7074662b7f925f10bd0248c487
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-17 17:40:44 -0300

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    return people.reduce((oldest, person) => {
+        const personAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+        oldest = personAge > oldestAge ? person : oldest;
+        return oldest;
+    })
 };
 
 // Do not edit below this line
