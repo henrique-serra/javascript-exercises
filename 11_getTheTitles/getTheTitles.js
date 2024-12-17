114fc48a1c1044f825316ca96c65c40ebeb676fb
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-17 18:24:21 +0000

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((array, book) => {
+        array.push(book.title);
+        return array;
+    }, [])
 };
 
 // Do not edit below this line
